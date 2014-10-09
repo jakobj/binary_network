3acfbfddeefcf9748ad8c77dd460a02cc2f3b550
Jakob Jordan <j.jordan@fz-juelich.de>
2014-10-09 16:57:03 +0200

    renamed some variables in simulate_eve

--- network.py
+++ network.py
@@ -35,7 +35,7 @@ def simulate_eve(W, b, tau, sinit, time, Nrec, l_N, l_F, Nrec_ui=0):
     record_s = True if Nrec > 0 else False
     record_ui = True if Nrec_ui > 0 else False
     N = len(b)
-    steps = int(np.ceil(1.*N*time/tau))
+    maxsteps = int(np.ceil(1.*N*time/tau))
     s = sinit
     step = 1
     maxrelsteps = int(np.ceil(1.1*Nrec*time/tau))
@@ -48,13 +48,13 @@ def simulate_eve(W, b, tau, sinit, time, Nrec, l_N, l_F, Nrec_ui=0):
     if record_ui:
         maxrelsteps_ui = int(np.ceil(1.1*Nrec_ui*time/tau))
         relstep_ui = 1
-        a_rec_ui = np.empty((maxrelsteps_ui, Nrec_ui))
+        a_ui = np.empty((maxrelsteps_ui, Nrec_ui))
         a_steps_ui = np.zeros(maxrelsteps_ui)
-        a_rec_ui[0] = np.dot(W[:Nrec_ui,:], s) + b[:Nrec_ui]
+        a_ui[0] = np.dot(W[:Nrec_ui,:], s) + b[:Nrec_ui]
         a_steps_ui[0] = 0.
     updates = list(zip(np.random.exponential(tau, N), np.random.permutation(np.arange(0, N))))
     hq.heapify(updates)
-    while step < steps:
+    while step < maxsteps:
         time, idx = hq.heappop(updates)
         idF = 0
         for Ni in l_N:
@@ -63,7 +63,7 @@ def simulate_eve(W, b, tau, sinit, time, Nrec, l_N, l_F, Nrec_ui=0):
             else:
                idF += 1
         if record_ui and idx < Nrec_ui:
-            a_rec_ui[relstep_ui] = np.dot(W[:Nrec_ui,:], s) + b[:Nrec_ui]
+            a_ui[relstep_ui] = np.dot(W[:Nrec_ui,:], s) + b[:Nrec_ui]
             a_steps_ui[relstep_ui] = time
             relstep_ui += 1
         ui = np.dot(W[idx,:], s) + b[idx]
@@ -80,8 +80,8 @@ def simulate_eve(W, b, tau, sinit, time, Nrec, l_N, l_F, Nrec_ui=0):
         a_steps = a_steps[:maxpos]
     if record_ui:
         maxpos_ui = np.where(a_steps_ui > 0.)[0][-1]
-        a_rec_ui = a_rec_ui[:maxpos_ui,:]
+        a_ui = a_ui[:maxpos_ui,:]
         a_steps_ui = a_steps_ui[:maxpos_ui]
-        return a_steps, a_s, a_steps_ui, a_rec_ui
+        return a_steps, a_s, a_steps_ui, a_ui
     else:
         return a_steps, a_s
