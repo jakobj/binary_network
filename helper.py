fixed bug in get_joints_sparse, which led to wrong normalization of joints, fixed small bug in simulate_eve_sparse, which led to wrong initial state

helper.py
@@ -150,7 +150,7 @@ def get_joints_sparse(sinit, a_s, steps_warmup, M=1):
             states[tuple(s)] = 0.
         for step, (idx, sidx) in enumerate(a_s):
             cstate[idx] = sidx
-            if step > steps_warmup:
+            if step >= steps_warmup:
                 states[tuple(cstate[i*N:(i+1)*N])] += 1
         states_sorted = np.array([it[1] for it in sorted(states.items())])
         a_joints[i,:] = 1.* states_sorted / steps_tot

network.py
@@ -92,7 +92,7 @@ def simulate_eve_sparse(W, b, tau, sinit, time, Nrec, l_N, l_F, beta=1.):
     assert(Nrec > 0)
     N = len(b)
     maxsteps = int(np.ceil(1.*N*time/tau))
-    s = sinit[:Nrec]
+    s = sinit.copy()
     step = 1
     maxrelsteps = int(np.ceil(1.1*Nrec*time/tau))
     relstep = 0