6d1af71b5fa50d805d1c5cd75e6d24f796132980
Jakob Jordan <j.jordan@fz-juelich.de>
2015-12-23 18:34:28 +0100

    improved documentation of unitginzburg

--- unitginzburgmeanfield.py
+++ unitginzburgmeanfield.py
@@ -15,14 +15,19 @@ DOI: 10.1371/journal.pcbi.1003428
 
 
 class BinaryMeanfield(object):
-    """
-    this module can calculate the stationary firing rate and mean
-    correlations in a network of binary neurons with an excitatory
-    and inhibitory population, from connectivity statistics
-    b is the bias vector (2d, corresponding to -1*threshold)
+    """this module allows one to calculate the stationary firing rates and
+    pairwise correlations in a network of binary neurons with
+    sigmoidal activation function from the connectivity matrix and
+    bias vector.
+
     """
 
     def __init__(self, J, b, beta):
+        """
+        J: connectivity matrix
+        b: bias vector
+        beta: inverse temperature (controls slope of gain function)
+        """
         self.J = J
         self.b = b
         self.N = len(b)
@@ -31,26 +36,33 @@ class BinaryMeanfield(object):
 
     def get_mu_meanfield(self, mu, C):
         """
-        Self-consistent rate
-        Formula (7) in Helias14
+        rates from rates and covariances
+        similar to Formula (7) in Helias14
+        mu: rate vector
+        C: covariance matrix
         """
+        # calculate input statistics from rates and covariances
         h_mu = self.get_mu_input(mu)
         h_mu += self.b
         h_sigma2 = self.get_sigma2_input(C)
-        # Taylor expansion of 1/(1 + exp(-beta*x)) around h_mu
+        # Taylor expansion
+        # of activation function under the integral after substitution
         eb = np.exp(self.beta * h_mu)
         C0 = eb / (eb + 1.)
+        C0 = 1. / (1. + np.exp(-1. * self.beta * h_mu))
         C2 = (eb ** 2 - eb) / (2. * (eb + 1.) ** 3) * self.beta ** 2
         C4 = (eb ** 4 - 11. * eb ** 3 + 11. * eb ** 2 - eb) \
             / (24. * (eb + 1.) ** 5) * self.beta ** 4
         mu = C0 - C2 * h_sigma2 - C4 * 3. * h_sigma2 ** 2
+
+        # Numerical integration
         # mu = np.empty(self.N)
         # for i in xrange(self.N):
-        #     # Numerical integration of activation function
         #     def f(x):
         #         return 1./(1. + np.exp(-self.beta * x)) * 1./np.sqrt(2. * np.pi * h_sigma2[i]) * np.exp(-(x - h_mu[i])**2 / (2. * h_sigma2[i]))
         #     mu[i], error = scint.quad(f, -50., 50.)
         #     assert(error < 1e-7), 'Integration error while determining mean activity.'
+
         # Sommerfeld expansion
         # WARNING: seems to fail due to size of h_sigma2
         # mu[i] = 0.5 * (1. + scsp.erf(h_mu[i]/np.sqrt(h_sigma2[i]))) \
@@ -60,16 +72,14 @@ class BinaryMeanfield(object):
     def get_mu_input(self, mu):
         """
         Mean input given presynaptic activity mu
-        Formula (4) in Helias14
+        mu: rate vector
         """
         return np.dot(self.J, mu)
 
     def get_sigma2_input(self, C):
         """
         Standard deviation of input given presynaptic activity mu
-        (and correlations C)
-        For C=None: formula (6) in Helias14
-        For C: formula (13) in Helias14
+        C: covariance matrix
         """
         assert(np.all(C.diagonal() >= 0.))
         sigma2_input = (np.dot(np.dot(self.J, C), self.J.T)).diagonal()
@@ -78,14 +88,17 @@ class BinaryMeanfield(object):
 
     def get_suszeptibility(self, mu, C):
         """
-        Suszeptibility (i.e., derivative of Gain function) for Gaussian
-        input with mean mu and standard deviation sigma
-        Formula (8) in Helias14
+        Suszeptibility (i.e., derivative of Gain function) from rates and covariances
+        see Formula (8) in Helias14
+        mu: rate vector
+        C: covariance matrix
         """
         h_mu = self.get_mu_input(mu)
         h_mu += self.b
         h_sigma2 = self.get_sigma2_input(C)
-        # Taylor expansion of beta/(1+exp(-beta*x))^2*exp(-beta*x) around h_mu
+
+        # Taylor expansion
+        # of derivative of activation function under the integral after substitution
         eb = np.exp(self.beta * h_mu)
         C0 = eb / (eb + 1) ** 2
         C2 = (eb ** 3 - 4. * eb ** 2 + eb) / \
@@ -93,9 +106,10 @@ class BinaryMeanfield(object):
         C4 = (eb ** 5 - 26 * eb ** 4 + 66 * eb ** 3 - 26 * eb ** 2 + eb) \
             / (24. * (eb + 1) ** 6) * self.beta ** 4
         S = self.beta * (C0 + C2 * h_sigma2 + C4 * 3. * h_sigma2 ** 2)
+
+        # Numerical integration
         # S = np.empty(self.N)
         # for i in xrange(self.N):
-        #     # Numerical integration of derivative of activation function
         #     def f(x):
         #         return self.beta / (np.exp(self.beta * x) + np.exp(-self.beta * x) + 2) * 1./np.sqrt(2. * np.pi * h_sigma2[i]) * np.exp(-(x - h_mu[i])**2 / (2. * h_sigma2[i]))
         #     S[i], error = scint.quad(f, -50., 50.)
@@ -104,21 +118,26 @@ class BinaryMeanfield(object):
 
     def get_w_meanfield(self, mu, C):
         """
-        Linearized population averaged weights
-        Formula (10) in Helias14
+        Linearized weights
+        see Formula (10) in Helias14
+        mu: rate vector
+        C: covariance matrix
         """
         S = np.diag(self.get_suszeptibility(mu, C))
         return np.dot(S, self.J)
 
-    def get_m_corr_iter(self, mu0, lamb, C=None):
-        """Calculate correlations iteratively from mean rates
+    def get_m_corr_iter(self, mu0, lamb, C0=None):
+        """Calculate correlations iteratively from rate vector
+        mu0: initial guess for rate vector
+        lamb: slowness parameter (controls convergence speed)
+        C: initial guess for covariance matrix
         """
         Dmu = 1e10
         Dc = 1e10
-        if C is None:
-            C = np.zeros((self.N, self.N))
+        if C0 is None:
+            C0 = np.zeros((self.N, self.N))
         mu = mu0.copy()
-        C = C.copy()
+        C = C0.copy()
         for i, m_i in enumerate(mu):
             C[i, i] = m_i * (1. - m_i)
         while Dmu > 1e-9 or Dc > 1e-9:
