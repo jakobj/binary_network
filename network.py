8306e19d11c6a1f6f2061075e541fe329f6ab5fb
Jakob Jordan <j.jordan@fz-juelich.de>
2015-12-23 13:40:36 +0100

    added possibility to record membrane potentials to simulate eve sparse

--- network.py
+++ network.py
@@ -106,7 +106,7 @@ def simulate_eve(W, b, tau, sinit, time, rNrec, l_N, l_F, Nrec_ui=0, beta=1.):
         return a_steps, a_s
 
 
-def simulate_eve_sparse(W, b, tau, s_init, Tmax, rNrec, l_N, l_F, beta=1.):
+def simulate_eve_sparse(W, b, tau, s_init, Tmax, rNrec, l_N, l_F, beta=1., rNrec_u=[0, 0]):
     """
     simulate a network of binary neurons
     (event driven, sparse recording of states)
@@ -123,27 +123,39 @@ def simulate_eve_sparse(W, b, tau, s_init, Tmax, rNrec, l_N, l_F, beta=1.):
     assert(len(l_N) == len(l_F))
     Nrec = rNrec[1] - rNrec[0]
     assert(Nrec >= 0)
+    Nrec_u = rNrec_u[1] - rNrec_u[0]
+    assert(Nrec_u >= 0)
+
     N = len(b)
     s = s_init.copy()
     maxsteps = int(np.ceil(1. * N * Tmax / tau))
 
-    # set up recording arrays
+    # set up recording array for states
     mean_recsteps = Nrec * Tmax / tau
     max_recsteps = int(np.ceil(mean_recsteps + 3 * np.sqrt(mean_recsteps)))  # Poisson process, mean + 3 * std
     a_s = np.empty(max_recsteps, dtype=int)
     a_times = np.zeros(max_recsteps)
     recstep = 0
+    # set up recording arrays for membrane potential
+    mean_recsteps_u = Nrec_u * Tmax / tau
+    max_recsteps_u = int(np.ceil(mean_recsteps_u + 3 * np.sqrt(mean_recsteps_u)))  # Poisson process, mean + 3 * std
+    a_u = np.empty((max_recsteps_u, Nrec_u), dtype=int)
+    a_times_u = np.zeros(max_recsteps_u)
+    recstep_u = 0
 
     # build lookup table for activation functions and recording
     F_lut = np.empty(N, dtype=int)
-    rec_lut = np.zeros(N, dtype=bool)
+    rec_s_lut = np.zeros(N, dtype=bool)
+    rec_u_lut = np.zeros(N, dtype=bool)
     idF = 0
     for idx in xrange(N):
         if idx >= l_N[idF]:
             idF += 1
         F_lut[idx] = idF
         if idx >= rNrec[0] and idx < rNrec[1]:
-            rec_lut[idx] = True
+            rec_s_lut[idx] = True
+        if idx >= rNrec_u[0] and idx < rNrec[1]:
+            rec_u_lut[idx] = True
 
     # choose initial update times
     updates = list(zip(np.random.exponential(tau, N),
@@ -155,14 +167,21 @@ def simulate_eve_sparse(W, b, tau, s_init, Tmax, rNrec, l_N, l_F, beta=1.):
     for _ in xrange(maxsteps):
         time, idx = hq.heappop(updates)
         ui = np.dot(W[idx, :], s) + b[idx]
+        if Nrec_u > 0 and rec_u_lut[idx]:
+            a_u[recstep_u] = np.dot(W[rNrec_u[0]:rNrec_u[1], :], s) + b[rNrec_u[0]:rNrec_u[1]]
+            a_times_u[recstep_u] = time
+            recstep_u += 1
         s[idx] = l_F[F_lut[idx]](ui, beta)
-        if rec_lut[idx]:
+        if Nrec > 0 and rec_s_lut[idx]:
             a_s[recstep] = bhlp.state_array_to_int(s[rNrec[0]:rNrec[1]])
             a_times[recstep] = time
             recstep += 1
         hq.heappush(updates, (time + np.random.exponential(tau), idx))
 
-    return a_times[:recstep], a_s[:recstep]
+    if Nrec_u == 0:
+        return a_times[:recstep], a_s[:recstep]
+    else:
+        return a_times[:recstep], a_s[:recstep], a_times_u[:recstep_u], a_u[:recstep_u]
 
 
 def simulate_eve_sparse_stim(W, b, tau, sinit, time, rNrec, l_N, l_F, l_pattern, beta=1.):
