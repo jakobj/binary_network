fix calculation of bias for BM; other minor changes

helper.py
@@ -115,7 +115,8 @@ def create_BM_biases_threshold_condition(N, mean_weight, mean_activity):
     eq. (5)
 
     """
-    return np.ones(N) * -1. * mean_weight * N * mean_activity
+    # for a Boltzmann machine with N units, we have (N - 1) inputs
+    return np.ones(N) * -1. * mean_weight * (N - 1) * mean_activity
 
 
 def create_BRN_weight_matrix(N, w, g, epsilon, gamma):
@@ -407,7 +408,7 @@ def get_beta_from_sigma_input(sigma_input):
     functions at zero
 
     """
-    return 4. / np.sqrt(2. * np.pi * sigma_input ** 2)
+    return np.sqrt(8. / (np.pi * sigma_input ** 2))
 
 
 def get_steps_warmup(rNrec, Twarmup, tau):

tests/test_helper.py
@@ -104,7 +104,7 @@ class HelperTestCase(unittest.TestCase):
             theo_hist_shared, hist_shared, decimal=2)
 
     def test_create_BM_weight_matrix(self):
-        N = 200
+        N = 300
         expected_diag = np.zeros(N)
 
         # test with uniform distribution